b8d13a916aadbf4c1137a602daa145ae16db344e
Bryce Hackel <34104885+bhackel@users.noreply.github.com>
2021-02-01 13:38:54 -0800

    Revert "Fixed rare button click crash"
    This reverts commit c24409a5009abfa37105ebb37523a653109bb06d.

--- auto_joiner.py
+++ auto_joiner.py
@@ -140,7 +140,7 @@ def switch_to_calendar_tab():
     calendar_button = wait_until_found(
         "button.app-bar-link > ng-include > svg.icons-calendar", 15)
     if calendar_button is not None:
-        browser.execute_script("arguments[0].click();", calendar_button)
+        calendar_button.click()
 
 
 def prepare_calendar_page():
